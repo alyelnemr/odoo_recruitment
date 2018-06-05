b013fc8528d98b6e6767eea1a83002bb7cf487d2
Ibrahim Moustafa Mohamed <Ibrahim.Moustafa@Andalusiagroup.net>
2018-06-05 14:33:51 +0300

    Updated build_test.ps1

--- build_test.ps1
+++ build_test.ps1
@@ -1,2 +1,2 @@
-cd "C:\Program Files (x86)\Odoo 11.0\server\odoo\custom addons\Recruitment\"
-cmd.exe /c 'build_test.bat'
+cd "C:\Program Files (x86)\Odoo 11.0\nssm\win64"
+.\nssm.exe set odoo-server-11.0 AppParameters "C:\PROGRA~2\ODOO11~1.0\server\odoo-bin" -d dbv11_recruitment -u recruitment_ads,sync_ldap_ads
