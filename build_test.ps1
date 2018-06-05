142022f46962b73f107111cb55a771e4d577b064
Ibrahim Moustafa Mohamed <Ibrahim.Moustafa@Andalusiagroup.net>
2018-06-05 14:45:26 +0300

    Updated build_test.ps1

--- build_test.ps1
+++ build_test.ps1
@@ -1,4 +1,2 @@
 "Run before"
-cd "C:\Program Files (x86)\Odoo 11.0\nssm\win64"
-.\nssm.exe set odoo-server-11.0 AppParameters "C:\PROGRA~2\ODOO11~1.0\server\odoo-bin" -d dbv11_recruitment -u recruitment_ads,sync_ldap_ads
 "Run after"
