752e49024da54596bc6dc359f1c9f7d4f39ac850
Ibrahim Moustafa Mohamed <Ibrahim.Moustafa@Andalusiagroup.net>
2018-06-10 17:18:02 +0300

    Updated build_test.ps1

--- build_test.ps1
+++ build_test.ps1
@@ -1,1 +1,1 @@
-C:\PROGRA~2\ODOO11~1.0\nssm\win64\nssm.exe set odoo-server-11.0 AppParameters "C:\PROGRA~2\ODOO11~1.0\server\odoo-bin" -d dbv11_recruitment -u recruitment_ads,sync_ldap_ads
+cmd /c 'dir'
