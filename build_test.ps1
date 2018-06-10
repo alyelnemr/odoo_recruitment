c92c4e979f72519218599fec0d841d325aadf8fb
Ibrahim Moustafa Mohamed <Ibrahim.Moustafa@Andalusiagroup.net>
2018-06-10 18:01:18 +0300

    Updated build_test.ps1

--- build_test.ps1
+++ build_test.ps1
@@ -1,3 +1,27 @@
-cd "C:\PROGRA~2\ODOO11~1.0\nssm\win64"
-dir
-nssm.exe
+$FilePath = "C:\PROGRA~2\ODOO11~1.0\nssm\win64\nssm.exe"
+$ArgumentList = ""
+  
+  $OFS = " "
+  $process = New-Object System.Diagnostics.Process
+  $process.StartInfo.FileName = $FilePath
+  $process.StartInfo.Arguments = $ArgumentList
+  $process.StartInfo.UseShellExecute = $false
+  $process.StartInfo.RedirectStandardOutput = $true
+  if ( $process.Start() ) {
+    $output = $process.StandardOutput.ReadToEnd() `
+      -replace "\r\n$",""
+    if ( $output ) {
+      if ( $output.Contains("`r`n") ) {
+        $output -split "`r`n"
+      }
+      elseif ( $output.Contains("`n") ) {
+        $output -split "`n"
+      }
+      else {
+        $output
+      }
+    }
+    $process.WaitForExit()
+    & "$Env:SystemRoot\system32\cmd.exe" `
+      /c exit $process.ExitCode
+  }
