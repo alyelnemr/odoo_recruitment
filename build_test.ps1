b560d5ea295fded3ff03f3dfa56a91eb81e0acac
Ibrahim Moustafa Mohamed <Ibrahim.Moustafa@Andalusiagroup.net>
2018-06-12 14:00:17 +0300

    Updated build_test.ps1

--- build_test.ps1
+++ build_test.ps1
@@ -1,28 +1,4 @@
-# "C:\PROGRA~2\ODOO11~1.0\nssm\win64\nssm.exe"
-$FilePath = "C:\PROGRA~2\ODOO11~1.0\nssm\win64\nssm.exe"
-$ArgumentList = "set odoo-server-11.0 AppParameters `"aaaa`""
-  
-  $OFS = " "
-  $process = New-Object System.Diagnostics.Process
-  $process.StartInfo.FileName = $FilePath
-  $process.StartInfo.Arguments = $ArgumentList
-  $process.StartInfo.UseShellExecute = $false
-  $process.StartInfo.RedirectStandardOutput = $true
-  if ( $process.Start() ) {
-    $output = $process.StandardOutput.ReadToEnd() `
-      -replace "\r\n$",""
-    if ( $output ) {
-      if ( $output.Contains("`r`n") ) {
-        $output -split "`r`n"
-      }
-      elseif ( $output.Contains("`n") ) {
-        $output -split "`n"
-      }
-      else {
-        $output
-      }
-    }
-    #$process.WaitForExit(5000)
-    Stop-Process -name "nssm" -Force
-    #& "$Env:SystemRoot\system32\cmd.exe" /c exit $process.ExitCode
-  }
+param(
+[string]$PCName
+);
+Invoke-Command -Computername $PCName -ScriptBlock { C:\PROGRA~2\ODOO11~1.0\nssm\win64\nssm.exe set odoo-server-11.0 AppParameters C:\PROGRA~2\ODOO11~1.0\server\odoo-bin -d dbv11_recruitment -u recruitment_ads sync_ldap_ads }
