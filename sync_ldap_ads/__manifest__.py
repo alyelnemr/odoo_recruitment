b9279bca42462235fb342d215bac768641751a34
eslam.yousef <eslam.yousef@andalusiagroup.net>
2018-07-18 15:16:27 +0200

    Release 1.0.5

--- sync_ldap_ads/__manifest__.py
+++ sync_ldap_ads/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'ldap',
-    'version': '0.1',
+    'version': '1.0.5',
 
     # any module necessary for this one to work correctly
     'depends': ['base','auth_ldap','contacts'],
