0331be3778179ea1c0daa66b15248c9a604f3fa8
eslam.yousef <eslam.yousef@andalusiagroup.net>
2018-09-04 18:01:20 +0200

    [WIP] 137860:Edit Offer http://ahq-tfs-01:8080/tfs/DefaultCollection/ODOO_Recruitment/_workitems?_a=edit&id=137860

--- recruitment_ads/models/offer.py
+++ recruitment_ads/models/offer.py
@@ -1,30 +1,46 @@
 from odoo import models, fields, api
-from datetime import datetime
 
 
 class Offer(models.Model):
     _name = 'hr.offer'
+    _inherit = ['mail.thread']
 
-    application_id = fields.Many2one('hr.applicant')
-    application_name = fields.Char(related='application_id.partner_name')
-    job_id = fields.Many2one('hr.job', string='Applied Job', related='application_id.job_id')
-    fixed_salary = fields.Float(string='Fixed Salary')
-    variable_salary = fields.Float(string='Variable Salary')
-    housing_allowance = fields.Float(string='Housing Allowance')
-    travel_allowance = fields.Float(string='Travel Allowance')
-    mobile_allowance = fields.Float(string='Mobile Allowance')
-    issue_date = fields.Date(string='Issue Date', default=fields.Date.today)
-    currency_id = fields.Many2one('res.currency',default=lambda self:self.env.user.company_id.currency_id)
-
-    @api.depends('application_id')
+    @api.depends('application_id', 'application_id.job_id', 'application_id.partner_id')
     def _offer_name(self):
-        offer_name = "Create Offer / " + self.application_id.job_id.name + " / " + self.application_id.partner_id.name
-        self.offer_name = offer_name
+        for offer in self:
+            name = offer.application_id.job_id.name + " / " + offer.application_id.partner_id.name
+            offer.name = name
+            offer_name = "Create Offer / " + name
+            offer.offer_name = offer_name
 
     @api.depends('fixed_salary', 'variable_salary', 'housing_allowance', 'travel_allowance', 'mobile_allowance')
     def _compute_total_package(self):
-        total_package = self.fixed_salary + self.variable_salary + self.housing_allowance + self.travel_allowance + self.mobile_allowance
-        self.total_package = total_package
+        for offer in self:
+            total_package = offer.fixed_salary + offer.variable_salary + \
+                            offer.housing_allowance + offer.travel_allowance + offer.mobile_allowance
+            offer.total_package = total_package
 
+    name = fields.Char(compute=_offer_name, string='Name')
     offer_name = fields.Char(compute=_offer_name)
+    application_id = fields.Many2one('hr.applicant')
+    application_name = fields.Char(related='application_id.partner_name')
+    job_id = fields.Many2one('hr.job', string='Applied Job', related='application_id.job_id')
+    fixed_salary = fields.Float(string='Fixed Salary', track_visibility='onchange')
+    variable_salary = fields.Float(string='Variable Salary', track_visibility='onchange')
+    housing_allowance = fields.Float(string='Housing Allowance', track_visibility='onchange')
+    travel_allowance = fields.Float(string='Travel Allowance', track_visibility='onchange')
+    mobile_allowance = fields.Float(string='Mobile Allowance', track_visibility='onchange')
+    issue_date = fields.Date(string='Issue Date', default=fields.Date.today)
+    hiring_date = fields.Date(string="Hiring Date", track_visibility='onchange')
+    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.user.company_id.currency_id)
     total_package = fields.Float(string='Total Package', compute=_compute_total_package, store=True)
+    state = fields.Selection(
+        [('offer', 'Offer'),
+         ('hold', 'Hold'),
+         ('pipeline', 'Pipeline'),
+         ('hired', 'Hired'),
+         ('not_join', 'Not Join'),
+         ('reject', 'Reject Offer')], default='offer', string="Hiring Status", track_visibility='onchange',
+        required=True)
+    comment = fields.Text(string='Notes')
+    reject_reason = fields.Selection([('1', 'r1')], string='Rejection Reason')
