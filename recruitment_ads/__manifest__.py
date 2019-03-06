a82c473fa8f34bc8f8832428cf88ebe87e9dc02e
eslam.yousef <Eslam.Yousef@andalusiagroup.net>
2019-03-06 11:20:44 +0200

    [FIX] Change version

--- recruitment_ads/__manifest__.py
+++ recruitment_ads/__manifest__.py
@@ -2,7 +2,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'Recruitment',
-    'version': '1.0.6',
+    'version': '2.1.0',
     'summary': 'Recruitment Management',
     'category': 'Employees',
     'author': 'AHBS Odoo Team',
