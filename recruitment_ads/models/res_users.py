53fb2140cd767a7a924fbba06b901cfd19bca2ee
eslam.yousef <Eslam.Yousef@andalusiagroup.net>
2020-01-13 17:07:15 +0200

    feature : add multi bussiness unit id in res.users

--- recruitment_ads/models/res_users.py
+++ recruitment_ads/models/res_users.py
@@ -1,7 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields ,api
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     business_unit_id = fields.Many2one('business.unit', string="Business Unit")
+    multi_business_unit_id = fields.Many2many('business.unit', string="Other Business Unit")
+
+    @api.onchange('business_unit_id')
+    def onchange_bu_ids(self):
+       self.multi_business_unit_id = False
