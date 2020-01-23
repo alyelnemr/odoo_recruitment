0cc89bf51aff2ec96304787d964e5ac816896862
tagelmaaly.mohamed <TagElMaaly.Mohamed@Andalusiagroup.net>
2020-01-23 17:05:08 +0200

    [Done] 302318:General Sheet Enhancementhttp://ahq-tfs-01:8080/tfs/DefaultCollection/ODOO_Recruitment/_workitems?_a=edit&id=302318

--- recruitment_ads/wizard/abstract_rec_report_wizard.py
+++ recruitment_ads/wizard/abstract_rec_report_wizard.py
@@ -5,50 +5,48 @@ from odoo.exceptions import ValidationError
 class AbstractRecruitmentReportWizard(models.AbstractModel):
     _name = 'abstract.rec.report.wizard'
 
-    date_from = fields.Date(string='Start Date', required=True, default=fields.Date.today)
-    date_to = fields.Date(string='End Date', required=True, default=fields.Date.today)
-
-    job_ids = fields.Many2many('hr.job', string='Job Position')
-
     def _get_bu_domain(self):
-        if self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') and not self.env.user.has_group('hr_recruitment.group_hr_recruitment_manager'):
-            domain=['|',('id', '=', self.env.user.business_unit_id.id), ('id','in',self.env.user.multi_business_unit_id.ids)
-                 ]
-
+        if self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') and not self.env.user.has_group(
+                'hr_recruitment.group_hr_recruitment_manager'):
+            domain = ['|', ('id', '=', self.env.user.business_unit_id.id),
+                      ('id', 'in', self.env.user.multi_business_unit_id.ids)]
         else:
-            domain=[]
+            domain = []
         return domain
 
     def _get_bu_default(self):
-        if self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') and not self.env.user.has_group('hr_recruitment.group_hr_recruitment_manager'):
-            bu = self.env['business.unit'].search(['|',('id', '=', self.env.user.business_unit_id.id), ('id','in',self.env.user.multi_business_unit_id.ids)])
-
+        if self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') and not self.env.user.has_group(
+                'hr_recruitment.group_hr_recruitment_manager'):
+            bu = self.env['business.unit'].search(['|', ('id', '=', self.env.user.business_unit_id.id),
+                                                   ('id', 'in', self.env.user.multi_business_unit_id.ids)])
         else:
             bu = self.env.user.business_unit_id
         return bu
 
-    bu_ids = fields.Many2many('business.unit', string='Business Unit',default =lambda self: self._get_bu_default(),domain=lambda self: self._get_bu_domain())
-
-
-
     @api.model
     def _get_current_login_user(self):
-     return [self.env.user.id]
-
-    recruiter_ids = fields.Many2many('res.users', string='Recruiter Responsible',default=_get_current_login_user)
-
+        return [self.env.user.id]
 
     @api.model
     def get_user(self):
         res_user = self.env.user
-        if res_user.has_group ('hr_recruitment.group_hr_recruitment_manager'):
+        if res_user.has_group('hr_recruitment.group_hr_recruitment_manager'):
             return "manager"
-        elif self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') and not self.env.user.has_group('hr_recruitment.group_hr_recruitment_manager'):
+        elif self.env.user.has_group(
+                'recruitment_ads.group_hr_recruitment_coordinator') and not self.env.user.has_group(
+            'hr_recruitment.group_hr_recruitment_manager'):
             return "coordinator"
         else:
             return "officer"
 
-    check_rec_manager = fields.Char(string="check field",default = get_user)
+    date_from = fields.Date(string='Start Date', required=True, default=fields.Date.today)
+    date_to = fields.Date(string='End Date', required=True, default=fields.Date.today)
+
+    job_ids = fields.Many2many('hr.job', string='Job Position')
+    bu_ids = fields.Many2many('business.unit', string='Business Unit', default=lambda self: self._get_bu_default(),
+                              domain=lambda self: self._get_bu_domain())
+    recruiter_ids = fields.Many2many('res.users', string='Recruiter Responsible', default=_get_current_login_user)
+    check_rec_manager = fields.Char(string="check field", default=get_user)
 
     @api.constrains('date_from', 'date_to')
     def check_dates(self):
@@ -60,7 +58,7 @@ class AbstractRecruitmentReportWizard(models.AbstractModel):
     def onchange_bu_ids(self):
         self.job_ids = False
 
-    @api.onchange('date_from', 'date_to','bu_ids')
+    @api.onchange('date_from', 'date_to', 'bu_ids')
     def onchange_job_ids(self):
         if self.date_from and self.date_to:
             changed_domain = [
@@ -79,10 +77,11 @@ class AbstractRecruitmentReportWizard(models.AbstractModel):
             jobs |= self.env['hr.applicant'].browse(changed_activity.mapped('res_id')).mapped('job_id')
 
             if self.bu_ids:
-                if self.check_rec_manager == 'coordinator' or self.check_rec_manager == 'manager' :
+                if self.check_rec_manager == 'coordinator' or self.check_rec_manager == 'manager':
                     bu_jobs = self.env['hr.job'].search([('business_unit_id', 'in', self.bu_ids.ids)])
                     recruiters = self.env['res.users'].search([('business_unit_id', 'in', self.bu_ids.ids)])
-                    return {'domain': {'job_ids': [('id', 'in', jobs.ids), ('id', 'in', bu_jobs.ids)],'recruiter_ids':[('id', 'in', recruiters.ids)]}}
+                    return {'domain': {'job_ids': [('id', 'in', jobs.ids), ('id', 'in', bu_jobs.ids)],
+                                       'recruiter_ids': [('id', 'in', recruiters.ids)]}}
 
                 else:
                     bu_jobs = self.env['hr.job'].search(
@@ -90,16 +89,21 @@ class AbstractRecruitmentReportWizard(models.AbstractModel):
                          ('other_recruiters_ids', 'in', self.env.user.id)])
                     return {'domain': {'job_ids': [('id', 'in', jobs.ids), ('id', 'in', bu_jobs.ids)]}}
             else:
-                if self.check_rec_manager == 'manager' :
+                if self.check_rec_manager == 'manager':
                     recruiters = self.env['res.users'].search([])
-                    return {'domain': {'job_ids': [('id', 'in', jobs.ids)],'recruiter_ids':[('id', 'in', recruiters.ids)]}}
+                    return {'domain': {'job_ids': [('id', 'in', jobs.ids)],
+                                       'recruiter_ids': [('id', 'in', recruiters.ids)]}}
 
                 elif self.check_rec_manager == 'coordinator':
-                    recruiters = self.env['res.users'].search(['|',('business_unit_id', '=', self.env.user.business_unit_id.id), ('business_unit_id','in',self.env.user.multi_business_unit_id.ids)])
-                    bu_jobs = self.env['hr.job'].search(['|',('business_unit_id', '=', self.env.user.business_unit_id.id),('business_unit_id', 'in', self.env.user.multi_business_unit_id.ids)])
+                    recruiters = self.env['res.users'].search(
+                        ['|', ('business_unit_id', '=', self.env.user.business_unit_id.id),
+                         ('business_unit_id', 'in', self.env.user.multi_business_unit_id.ids)])
+                    bu_jobs = self.env['hr.job'].search(
+                        ['|', ('business_unit_id', '=', self.env.user.business_unit_id.id),
+                         ('business_unit_id', 'in', self.env.user.multi_business_unit_id.ids)])
                     return {'domain': {'job_ids': [('id', 'in', jobs.ids), ('id', 'in', bu_jobs.ids)],
                                        'recruiter_ids': [('id', 'in', recruiters.ids)]}}
                 else:
                     rec_jobs = self.env['hr.job'].search(
-                        ['|', ('user_id', '=', self.env.user.id),('other_recruiters_ids', 'in', self.env.user.id)])
+                        ['|', ('user_id', '=', self.env.user.id), ('other_recruiters_ids', 'in', self.env.user.id)])
                     return {'domain': {'job_ids': [('id', 'in', jobs.ids), ('id', 'in', rec_jobs.ids)]}}
