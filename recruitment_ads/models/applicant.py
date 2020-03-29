a5dccfd549b80ac383608c4b51fe859d22bd084e
Aly Abdel Hamid <aly5elnemr@gmail.com>
2020-03-29 15:59:50 +0200

    Revert "[Done] Bug Fixing: have_cv dose not behave properly http://ahq-tfs-01:8080/tfs/DefaultCollection/ODOO_Recruitment/_workitems?_a=edit&id=320583&fullScreen=false" tagelmaaly.mohamed 29-Mar-2020 3:35 PM

--- recruitment_ads/models/applicant.py
+++ recruitment_ads/models/applicant.py
@@ -74,14 +74,13 @@ class Applicant(models.Model):
     linkedin = fields.Char(string='LinkedIn Link', readonly=False)
     have_cv = fields.Boolean(srting='Have CV', compute='_get_attachment', default=False, store=True)
 
-    @api.multi
-    @api.depends('attachment_ids.res_id')
+    @api.one
+    @api.depends('attachment_ids')
     def _get_attachment(self):
-        for record in self:
-            if record.attachment_ids:
-                    record.have_cv = True
-            else:
-                    record.have_cv = False
+        if self.attachment_ids:
+            self.have_cv = True
+        else:
+            self.have_cv = False
 
     @api.one
     @api.depends('job_id.job_title_id.job_code', 'partner_mobile', 'partner_name', 'serial')
