d8c9d1e985f002b73add8a2735b2e15521f2126d
esraa-elmasry <esraa-elmasry@andalusiagroup.net>
2020-05-06 21:56:18 +0200

    DONE: compliance report

--- recruitment_ads/reports/compliance_report_xlsx.py
+++ recruitment_ads/reports/compliance_report_xlsx.py
@@ -12,15 +12,17 @@ class ComplianceReportXslx(models.AbstractModel):
         sheets = []
         sheets.append({
             'Compliance Report': {
-                0: {'header': _('Recruiter Responsible'), 'field': 'user_id', 'width': 20, 'type': 'many2one'},
-                1: {'header': _('Recruiter BU'), 'field': 'generated_by_bu_id', 'width': 20, 'type': 'many2one'},
-                2: {'header': _('Total applications'), 'field': 'total_application', 'width': 20},
-                3: {'header': _('Total calls'), 'field': 'no_calls', 'width': 20},
-                4: {'header': _('Done calls'), 'field': 'done_call', 'width': 20},
-                5: {'header': _('Total Interviews'), 'field': 'total_interview', 'width': 20},
-                6: {'header': _('Done Interviews'), 'field': 'done_interview', 'width': 20},
-                7: {'header': _('Total Offers'), 'field': 'total_offers', 'width': 20},
-                8: {'header': _('Hired'), 'field': 'no_hire', 'width': 20},
+                0: {'header': _('Recruiter Responsible'), 'field': 'user_id', 'width': 20},
+                1: {'header': _('Recruiter BU'), 'field': 'user_bu', 'width': 20, 'type': 'many2one'},
+                2: {'header': _('Total Applications'), 'field': 'total_application', 'width': 20},
+                3: {'header': _('App/Cvs'), 'field': 'no_cv', 'width': 20},
+                4: {'header': _('App/Assessment'), 'field': 'no_assess', 'width': 20},
+                5: {'header': _('Total Calls'), 'field': 'no_calls', 'width': 20},
+                6: {'header': _('Done Calls'), 'field': 'done_call', 'width': 20},
+                7: {'header': _('Total Interviews'), 'field': 'total_interview', 'width': 20},
+                8: {'header': _('Done Interviews'), 'field': 'done_interview', 'width': 20},
+                9: {'header': _('Total Offers'), 'field': 'total_offers', 'width': 20},
+                10: {'header': _('Hired'), 'field': 'no_hire', 'width': 20}
 
             }
         })
@@ -32,61 +34,74 @@ class ComplianceReportXslx(models.AbstractModel):
             domain = [('create_date', '>=', report.date_from + ' 00:00:00'),
                       ('create_date', '<=', report.date_to + ' 23:59:59'),
                       ]
-            for rec in set(report.application_ids.mapped('create_uid')):
-                total_app = report.application_ids.filtered(lambda x: x.create_uid == rec).mapped(id)
-                call_domain =[
-                    ('active', '=', False),
-                    ('res_model', '=', 'hr.applicant'),
-                    ('real_create_uid', '=', rec.id),
-                    ('activity_type_id', 'in', (2, 6, 7)),
-                    ('id', 'in', report.application_ids.ids),
-                ]
-                interviews_domain = [
-                    ('active', '=', False),
-                    ('res_model', '=', 'hr.applicant'),
-                    ('real_create_uid', '=', rec.id),
-                    ('activity_type_id', '=', 5),
-                    ('id','in',report.application_ids.ids),]
 
-                offers_domain = [('create_uid', '=', rec.id),
-                                 ('application_id','in',report.application_ids.ids),
-                                 ]
-                # if report.bu_ids:
-                #     offers_domain.append(('business_unit_id','in',report.bu_ids.ids))
-                #
+            activity_calls = [('active', '=', False),
+                               ('res_model', '=', 'hr.applicant'),
+                              ('activity_type_id', 'in' , (2, 6, 7))
+                               ]
 
-                call_domain.extend(domain)
-                total_calls = self.env['mail.activity'].search(call_domain)
-                no_done_calls=total_calls.filtered(lambda x: x.call_result_id != False)
+            activity_interviews = [('active', '=', False),
+                                   ('res_model', '=', 'hr.applicant'),
+                                   ('activity_type_id', '=' , 5),
+                               ]
+            app_domain = []
+            offers_domain = []
+            if report.bu_ids:
+                jobs=self.env['hr.job'].search([('business_unit_id', 'in', report.bu_ids.ids)])
+                applications = self.env['hr.applicant'].search([('job_id', 'in', jobs.ids)])
+                app_domain.append(('job_id','in',jobs.ids),)
+                offers_domain.append(('business_unit_id','in',report.bu_ids.ids),)
+                activity_interviews.append(('res_id','in',applications.ids),)
+                activity_calls.append(('res_id','in',applications.ids),)
 
-
-                interviews_domain.extend(domain)
-                interviews = self.env['mail.activity'].search(interviews_domain)
+            app_domain.extend(domain)
+            activity_calls.extend(domain)
+            activity_interviews.extend(domain)
+            offers_domain.extend(domain)
+            for recruiter in report.recruiter_ids.ids:
+                app_domain.append(('create_uid','=',recruiter),)
+                activity_calls.append(('real_create_uid', '=', recruiter), )
+                activity_interviews.append(('real_create_uid', '=', recruiter), )
+                offers_domain.append(('create_uid', '=', recruiter), )
+                total_app = self.env['hr.applicant'].search(app_domain)
+                attachments = self.env['ir.attachment'].search([('res_model', '=', 'hr.applicant'), ('res_id', 'in', total_app.ids)])
+                cvs = attachments.filtered(lambda x: x.attachment_type == 'cv')
+                asses = attachments.filtered(lambda x: x.attachment_type == 'assessment')
+                total_calls = self.env['mail.activity'].search(activity_calls)
+                no_done_calls = total_calls.filtered(lambda x: x.call_result_id != False)
+                interviews = self.env['mail.activity'].search(activity_interviews)
                 done_interviews = interviews.filtered(lambda x: x.interview_result != False)
-
-                offers_domain.extend(domain)
                 offers = self.env['hr.offer'].search(offers_domain)
                 hired = offers.filtered(lambda x: x.state == 'hired')
+                del app_domain[-1]
+                del activity_calls[-1]
+                del activity_interviews[-1]
+                del offers_domain[-1]
+                total_app = str(len(total_app))
+                cvs_no = str(len(cvs))
+                ass_no = str(len(asses))
+                total_calls = str(len(total_calls))
+                no_done_calls = str(len(no_done_calls))
+                interviews = str(len(interviews))
+                done_interviews = str(len(done_interviews))
+                offers = str(len(offers))
+                hired = str(len(hired))
+                recruiter_obj = self.env['res.users'].browse(recruiter)
+                self.write_line(
+                    ComplianceWrapper(total_app, cvs_no, ass_no, recruiter_obj, total_calls, no_done_calls, interviews,done_interviews,offers, hired),'Compliance Report')
+            report.recruiter_ids = False
 
-                total_app=str(len(total_app))
-                total_calls=str(len(total_calls))
-                no_done_calls=str(len(no_done_calls))
-                interviews=str(len(interviews))
-                done_interviews=str(len(done_interviews))
-                offers=str(len(offers))
-                hired=str(len(hired))
 
-
-                self.write_line(ComplianceWrapper(total_app,rec,total_calls,no_done_calls,interviews,done_interviews,
-                                                  offers,hired), 'Compliance Report')
 
 
 
 class ComplianceWrapper:
-    def __init__(self,total_app,recruiter,total_calls,done_calls,no_interviews,done_interviews,offers,hired):
-        self.user_id = recruiter
-        self.generated_by_bu_id = recruiter.business_unit_id
+    def __init__(self,total_app,cvs_no,ass_no,recruiter,total_calls,done_calls,no_interviews,done_interviews,offers,hired):
+        self.user_id = recruiter.name
+        self.user_bu = recruiter.business_unit_id
         self.total_application= total_app
+        self.no_cv=cvs_no
+        self.no_assess=ass_no
         self.no_calls = total_calls
         self.done_call=done_calls
         self.total_interview = no_interviews
--- recruitment_ads/wizard/compliance_report_wizard.py
+++ recruitment_ads/wizard/compliance_report_wizard.py
@@ -8,7 +8,6 @@ class ComplianceReportWizard(models.TransientModel):
     date_to = fields.Date(string='End Date', required=True, default=fields.Date.today)
     recruiter_ids = fields.Many2many('res.users', string='Recruiter Responsible')
     bu_ids = fields.Many2many('business.unit', string='Business Unit')
-    application_ids = fields.Many2many('hr.applicant')
 
     @api.constrains('date_from', 'date_to')
     def check_dates(self):
@@ -19,34 +18,22 @@ class ComplianceReportWizard(models.TransientModel):
     @api.onchange('bu_ids')
     def onchange_bu_ids(self):
         self.recruiter_ids = False
-        if not self.bu_ids:
-            recruiters = self.env['res.users'].search([])
+        if  self.bu_ids:
+            recruiters = self.env['res.users'].search(
+                ['|', ('business_unit_id', 'in', self.bu_ids.ids), ('multi_business_unit_id', 'in', self.bu_ids.ids)])
         else:
-            recruiters = self.env['res.users'].search([('business_unit_id', 'in', self.bu_ids.ids)])
+            recruiters = self.env['res.users'].search([])
         return {'domain': {'recruiter_ids': [('id', 'in', recruiters.ids)]}}
 
     @api.multi
     def button_export_xlsx(self):
         self.ensure_one()
-        no_records = True
-        domain = [
-            ('create_date', '>=', self.date_from + ' 00:00:00'),
-            ('create_date', '<=', self.date_to + ' 23:59:59'),
-        ]
-        if self.recruiter_ids:
-            domain.append(('create_uid', 'in', self.recruiter_ids.ids))
         if self.bu_ids:
-            bu_jobs = self.env['hr.job'].search([('business_unit_id', 'in', self.bu_ids.ids)])
-            domain.append(('job_id', 'in', bu_jobs.ids))
-
-        applications = self.env['hr.applicant'].search(domain, order='create_date desc')
-        if applications:
-            no_records = False
-            self.application_ids =[(6, 0, applications.ids)]
-
-        if no_records:
-            raise ValidationError(_("No record to display"))
-
+            if not self.recruiter_ids:
+                self.recruiter_ids = self.env['res.users'].search(['|',('business_unit_id', 'in', self.bu_ids.ids),('multi_business_unit_id','in',self.bu_ids.ids)])
+        else:
+            if not self.recruiter_ids:
+                self.recruiter_ids = self.env['res.users'].search([])
 
 
         report = self.env.ref('recruitment_ads.action_compliance_report_xlsx')
