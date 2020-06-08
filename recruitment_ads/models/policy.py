83629c7a6e5a3af1e755de859876d742f5086169
tagelmaaly.mohamed <TagElMaaly.Mohamed@Andalusiagroup.net>
2020-06-08 15:56:19 +0200

    [Fix]  [No user can delete HR Policy even the Admin]  325603:Validate Multiple Application      http://ahq-tfs-01:8080/tfs/DefaultCollection/ODOO_Recruitment/_workitems?_a=edit&id=325603

--- recruitment_ads/models/policy.py
+++ recruitment_ads/models/policy.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class HRPolicy(models.Model):
@@ -8,3 +9,9 @@ class HRPolicy(models.Model):
     day = fields.Integer(string="Days")
     month = fields.Integer(string="Months")
     year = fields.Integer(string="Years")
+
+    @api.multi
+    def unlink(self):
+        for line in self:
+            raise ValidationError(_('Cannot Delete HR Policy.'))
+        return super(HRPolicy, self).unlink()
