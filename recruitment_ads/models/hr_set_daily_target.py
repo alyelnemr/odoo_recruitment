b6c5032873028ba410a4a2a9e541acf46322231f
Aly.Abdelhamid <Aly.Abdelhamid@andalusiagroup.net>
2020-07-06 14:41:28 +0200

    [FIXED] [fix lik of Generate daily target for officer and coordinator] tagelmaaly.mohamed 2 minutes ago 6d221f94

--- recruitment_ads/models/hr_set_daily_target.py
+++ recruitment_ads/models/hr_set_daily_target.py
@@ -231,4 +231,4 @@ class HRSetDailyTargetLine(models.Model):
         #                                                       'generate_daily_target_report_wizard_action')
         # menu = self.env.ref('recruitment_ads.hr_recruitment_generate_daily_target_report').id
         # return 'web#menu_id=' + str(menu) + '&action=' + str(action.get('id', '')) + '&id=' + str(res.id)
-        return 'mail/view?model=generate.daily.target.report.wizard&res_id=' + str(res.id)
+        return 'web#id=' + str(res.id) + '&view_type=form&model=generate.daily.target.report.wizard'
