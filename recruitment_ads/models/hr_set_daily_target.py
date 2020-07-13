295c7745ec8156d06cc033662924b274a5281ac4
esraa-elmasry <esraa-elmasry@andalusiagroup.net>
2020-07-13 13:31:32 +0200

    [WIP] set monthly target

--- recruitment_ads/models/hr_set_daily_target.py
+++ recruitment_ads/models/hr_set_daily_target.py
@@ -201,30 +201,30 @@ class HRSetDailyTargetLine(models.Model):
         # for line in self:
         self.weight = self.level_id.weight
         self.cvs = self.level_id.cv
-    #
-    # @api.multi
-    # def write(self, vals):
-    #     res = super(HRSetDailyTargetLine, self).write(vals)
-    #     self.send_daily_target_mail(vals)
-    #     return res
-    #
-    # def send_daily_target_mail(self, data):
-    #     if data:
-    #         self.send_mail_set_daily_target()
-    #
-    # @api.multi
-    # def send_mail_set_daily_target(self):
-    #     template = self.env.ref('recruitment_ads.set_daily_target_line_email_template')
-    #     self.env['mail.template'].browse(template.id).send_mail(self.id)
-    #
-    # def get_mail_url(self):
-    #     self.ensure_one()
-    #     res = self.env['generate.daily.target.report.wizard'].with_env(self.env(user=self.recruiter_id)).create({
-    #         'date_from': self.name,
-    #         'date_to': self.name,
-    #         'job_ids': [(6, 0, [self.job_position_id.id] or [])] if self.job_position_id else False,
-    #         'recruiter_ids': [(6, 0, [self.recruiter_id.id] or [])] if self.recruiter_id else False,
-    #     })
-    #     action = self.env['ir.actions.act_window'].for_xml_id('recruitment_ads',
-    #                                                           'generate_daily_target_report_wizard_action')
-    #     return 'web#action=' + str(action.get('id')) + '&id=' + str(res.id)
+
+    @api.multi
+    def write(self, vals):
+        res = super(HRSetDailyTargetLine, self).write(vals)
+        self.send_daily_target_mail(vals)
+        return res
+
+    def send_daily_target_mail(self, data):
+        if data:
+            self.send_mail_set_daily_target()
+
+    @api.multi
+    def send_mail_set_daily_target(self):
+        template = self.env.ref('recruitment_ads.set_daily_target_line_email_template')
+        self.env['mail.template'].browse(template.id).send_mail(self.id)
+
+    def get_mail_url(self):
+        self.ensure_one()
+        res = self.env['generate.daily.target.report.wizard'].with_env(self.env(user=self.recruiter_id)).create({
+            'date_from': self.name,
+            'date_to': self.name,
+            'job_ids': [(6, 0, [self.job_position_id.id] or [])] if self.job_position_id else False,
+            'recruiter_ids': [(6, 0, [self.recruiter_id.id] or [])] if self.recruiter_id else False,
+        })
+        action = self.env['ir.actions.act_window'].for_xml_id('recruitment_ads',
+                                                              'generate_daily_target_report_wizard_action')
+        return 'web#action=' + str(action.get('id')) + '&id=' + str(res.id)
--- recruitment_ads/models/hr_set_monthly_target.py
+++ recruitment_ads/models/hr_set_monthly_target.py
@@ -1,7 +1,9 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-from datetime import datetime
+from datetime import datetime ,timedelta
 from dateutil.relativedelta import relativedelta
+import datetime as datetime_now
+    #, timedelta
 
 class HRSetMonthlyTarget(models.Model):
     _name = 'hr.set.monthly.target'
@@ -114,8 +116,27 @@ class HRSetMonthlyTarget(models.Model):
                     })
 
         if len(self.line_ids) == 0:
+            self.env.cr.rollback()
             raise ValidationError('Please Set Monthly Target first before saving')
+
         return True
+
+    @api.model
+    def create(self, vals):
+        # if not vals['line_ids'] :
+        #     raise ValidationError('Please Set Monthly Target first before saving')
+        if not (self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') or self.env.user.has_group(
+                'hr_recruitment.group_hr_recruitment_manager')):
+            raise ValidationError(_('You are not allowed to set targets'))
+        res = super(HRSetMonthlyTarget, self).create(vals)
+        return res
+
+    @api.multi
+    def write(self, vals):
+        if not (self.env.user.has_group('recruitment_ads.group_hr_recruitment_coordinator') or self.env.user.has_group(
+                'hr_recruitment.group_hr_recruitment_manager')):
+            raise ValidationError(_('You are not allowed to set targets'))
+        return super(HRSetMonthlyTarget, self).write(vals)
 
 
 class HRSetMonthlyTargetLine(models.Model):
@@ -125,29 +146,29 @@ class HRSetMonthlyTargetLine(models.Model):
 
     start_date = fields.Date(required=True, track_visibility='always')
     recruiter_bu_id = fields.Many2one('business.unit', string='Recruiter BU',
-                                      track_visibility='always')
+                                      track_visibility='always',readonly=True)
     recruiter_id = fields.Many2one('res.users', required=True,  string='Recruiter Responsible',
-                                   track_visibility='always')
-    bu_id = fields.Many2one('business.unit', string='BU', required=True,  track_visibility='always')
+                                   track_visibility='always',readonly=True)
+    bu_id = fields.Many2one('business.unit', string='BU', required=True,  track_visibility='always',readonly=True)
     department_id = fields.Many2one('hr.department', string='Department', required=True,
-                                    track_visibility='always')
+                                    track_visibility='always',readonly=True)
     section_id = fields.Many2one('hr.department', string='Section',
-                                 track_visibility='always')
-    job_id = fields.Many2one('job.title', string='Position', required=True,  track_visibility='always')
+                                 track_visibility='always',readonly=True)
+    job_id = fields.Many2one('job.title', string='Position', required=True,  track_visibility='always',readonly=True)
     job_position_id = fields.Many2one('hr.job', string='Position from target',
                                       track_visibility='always')
     level_id = fields.Many2one('job.level', string='Level', track_visibility='always')
 
     offer_target = fields.Integer(string="Offer Target", track_visibility='always', default=0)
     hire_target = fields.Integer(string="Hire Target", track_visibility='always', default=0)
-    vacant = fields.Integer(string="Vacant", track_visibility='always', default=0)
+    vacant = fields.Integer(string="Vacant", track_visibility='always', default=0,readonly=True,compute='_compute_vacant')
     replacement_emp = fields.Integer(string="Replacement", track_visibility='always', default=0)
     man_power = fields.Integer(string="MP", track_visibility='always', default=0)
     current_emp = fields.Integer(string="Current", track_visibility='always', default=0)
-    expecting_offer_date = fields.Date( track_visibility='always',string='Expecting Offer Date')
-    expecting_hire_date = fields.Date( track_visibility='always',string='Expecting Hire Date')
-    offer_weight = fields.Integer( track_visibility='always',string='Offer Weight')
-    hire_weight = fields.Integer(track_visibility='always', string='Hire Weight')
+    expecting_offer_date = fields.Date( track_visibility='always',string='Expecting Offer Date',readonly=True,compute ='compute_offer_hire_expecting_date')
+    expecting_hire_date = fields.Date( track_visibility='always',string='Expecting Hire Date',readonly=True ,compute ='compute_offer_hire_expecting_date')
+    offer_weight = fields.Integer( track_visibility='always',string='Offer Weight',readonly=True)
+    hire_weight = fields.Integer(track_visibility='always', string='Hire Weight',readonly=True)
 
 
     target_id = fields.Many2one('hr.set.monthly.target', string='Set Monthly Target', track_visibility='always')
@@ -158,37 +179,62 @@ class HRSetMonthlyTargetLine(models.Model):
 
     @api.onchange('level_id')
     def _compute_level(self):
-        self.hire_weight = self.offer_weight = self.level_id.weight
+        for record in self:
+          record.hire_weight = record.offer_weight = record.level_id.weight
 
-    @api.model
-    def create(self, vals):
-        res = super(HRSetMonthlyTargetLine, self).create(vals)
-        res.send_monthly_target_mail(vals)
-        return res
+    @api.onchange('start_date')
+    def _validation_start_date(self):
+        for record in self:
+           if record.start_date > record.target_id.date_to or record.start_date < record.target_id.date_from :
+               raise ValidationError('Cannot accept date before Start Date of the Target OR after the Target date .')
 
-    @api.multi
-    def write(self, vals):
-        res = super(HRSetMonthlyTargetLine, self).write(vals)
-        self.send_monthly_target_mail(vals)
-        return res
+    @api.depends('man_power','current_emp')
+    def _compute_vacant(self):
+        for record in self:
+            record.vacant = record.man_power - record.current_emp
 
-    def send_monthly_target_mail(self, data):
-        if data:
-            self.send_mail_set_monthly_target()
+    @api.depends('start_date','level_id')
+    def compute_offer_hire_expecting_date(self):
+        hr_policy = self.env['hr.policy'].search([('hr_policy_type','=', 'offer_and_hire')], limit=1)
+        if hr_policy:
+            for policy in hr_policy.offer_and_hire_level:
+                for record in self:
+                    if policy.level == record.level_id:
+                        offer_days = int(policy.offer)
+                        hire_days = int(policy.hire)
+                        date_1 = datetime_now.datetime.strptime(record.start_date, "%Y-%m-%d")
+                        record.expecting_offer_date = date_1 + datetime_now.timedelta(days = offer_days)
+                        record.expecting_hire_date = date_1 + datetime_now.timedelta(days=hire_days)
 
-    @api.multi
-    def send_mail_set_monthly_target(self):
-        template = self.env.ref('recruitment_ads.set_monthly_target_line_email_template')
-        self.env['mail.template'].browse(template.id).send_mail(self.id)
-
-    def get_mail_url(self):
-        self.ensure_one()
-        res = self.env['generate.monthly.target.report.wizard'].with_env(self.env(user=self.recruiter_id)).create({
-            'date_from': self.name,
-            'date_to': self.name,
-            'job_ids': [(6, 0, [self.job_position_id.id] or [])] if self.job_position_id else False,
-            'recruiter_ids': [(6, 0, [self.recruiter_id.id] or [])] if self.recruiter_id else False,
-        })
-        action = self.env['ir.actions.act_window'].for_xml_id('recruitment_ads',
-                                                              'generate_monthly_target_report_wizard_action')
-        return 'web#action=' + str(action.get('id')) + '&id=' + str(res.id)
+    # @api.model
+    # def create(self, vals):
+    #     res = super(HRSetMonthlyTargetLine, self).create(vals)
+    #     res.send_monthly_target_mail(vals)
+    #     return res
+    #
+    # @api.multi
+    # def write(self, vals):
+    #     res = super(HRSetMonthlyTargetLine, self).write(vals)
+    #     self.send_monthly_target_mail(vals)
+    #     return res
+    #
+    # def send_monthly_target_mail(self, data):
+    #     if data:
+    #         self.send_mail_set_monthly_target()
+    #
+    # @api.multi
+    # def send_mail_set_monthly_target(self):
+    #     template = self.env.ref('recruitment_ads.set_monthly_target_line_email_template')
+    #     self.env['mail.template'].browse(template.id).send_mail(self.id)
+    #
+    # def get_mail_url(self):
+    #     self.ensure_one()
+    #     res = self.env['generate.monthly.target.report.wizard'].with_env(self.env(user=self.recruiter_id)).create({
+    #         'date_from': self.name,
+    #         'date_to': self.name,
+    #         'job_ids': [(6, 0, [self.job_position_id.id] or [])] if self.job_position_id else False,
+    #         'recruiter_ids': [(6, 0, [self.recruiter_id.id] or [])] if self.recruiter_id else False,
+    #     })
+    #     action = self.env['ir.actions.act_window'].for_xml_id('recruitment_ads',
+    #                                                           'generate_monthly_target_report_wizard_action')
+    #     return 'web#action=' + str(action.get('id')) + '&id=' + str(res.id)
