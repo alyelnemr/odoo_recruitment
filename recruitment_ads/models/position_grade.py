c14ddc64baefe78c14b01688d85ae5d8b81b4e9a
marina.mikhael <Marina.Mikhael@Andalusiagroup.net>
2020-07-20 11:03:22 +0200

    [ADD] Item 345441*:Offer Enhancement >> add sequence

--- recruitment_ads/models/position_grade.py
+++ recruitment_ads/models/position_grade.py
@@ -5,5 +5,7 @@ from odoo import models, fields
 class PositionGrade(models.Model):
     _name = "position.grade"
     _inherit = ['mail.thread']
+    _order = 'sequence asc'
 
     name = fields.Char(string='Name', required=True)
+    sequence = fields.Integer(default=10)
--- recruitment_ads/models/salary_scale.py
+++ recruitment_ads/models/salary_scale.py
@@ -5,5 +5,7 @@ from odoo import models, fields
 class SalaryScale(models.Model):
     _name = "salary.scale"
     _inherit = ['mail.thread']
+    _order = 'sequence asc'
 
     name = fields.Char(string='Name', required=True)
+    sequence = fields.Integer(default=10)
