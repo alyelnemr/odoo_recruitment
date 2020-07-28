b062a0cbc1c0cf84f4b701fbba5ddfc44218d149
marina.mikhael <Marina.Mikhael@Andalusiagroup.net>
2020-07-28 18:18:35 +0200

    [ADD]   Item 334835:Setup Approval Cycle Check that the system should validate multiple Approval Cycle  Duplicating the Filtration Criteria either when creating /editing

--- recruitment_ads/models/hr_setup_approval_cycle.py
+++ recruitment_ads/models/hr_setup_approval_cycle.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, SUPERUSER_ID
+from odoo import models, fields, api, SUPERUSER_ID, _
+from odoo.exceptions import ValidationError
 
 
 class HRSetupApprovalCycle(models.Model):
@@ -14,7 +15,7 @@ class HRSetupApprovalCycle(models.Model):
     position_grade_id = fields.Many2many('position.grade', string='Position Grade', required=True)
     salary_scale_id = fields.Many2one('salary.scale', string='Salary Scale', required=True)
     offer_type = fields.Selection([('normal_offer', 'Normal Offer'),
-                                   ('nursing_offer', 'Medical/Nursing Offer'), ],
+                                   ('nursing_offer', 'Nursing Offer'), ],
                                   string="Offer Type", default="normal_offer", required=True)
     no_of_approval = fields.Integer('No Of Approval', compute='_compute_no_of_approval')
     approval_list_ids = fields.One2many('hr.setup.approval.cycle.users', 'approval_cycle_id', 'Approval List')
@@ -25,6 +26,16 @@ class HRSetupApprovalCycle(models.Model):
 
     @api.model
     def create(self, vals):
+        is_exist = self.search([
+            ('recruiter_bu', '=', vals.get('recruiter_bu', False)),
+            ('position_grade_id', 'in',
+             vals.get('position_grade_id')[0][2] if vals.get('position_grade_id', False) else False),
+            ('salary_scale_id', '=', vals.get('salary_scale_id', False)),
+            ('offer_type', '=', vals.get('offer_type', False))
+        ], limit=1)
+        if is_exist:
+            raise ValidationError(
+                _('There is exist approval cycle (%s) with the same filtration criteria.') % is_exist.name)
         approval_list = []
         stage_id = self.env['hr.master.approval.group.state'].search([], limit=1)
         for approval_user in self.env['hr.master.approval.group'].search([]):
@@ -35,6 +46,22 @@ class HRSetupApprovalCycle(models.Model):
         vals['approval_list_ids'] = approval_list
         vals['name'] = self.env.ref('recruitment_ads.sequence_approval_cycle').next_by_id()
         return super(HRSetupApprovalCycle, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        for approval_cycle in self:
+            is_exist = approval_cycle.search([
+                ('recruiter_bu', '=', vals.get('recruiter_bu', False) or approval_cycle.recruiter_bu),
+                ('position_grade_id', 'in',
+                 vals.get('position_grade_id')[0][2] if vals.get('position_grade_id',
+                                                                 False) else approval_cycle.position_grade_id.ids),
+                ('salary_scale_id', '=', vals.get('salary_scale_id', False) or approval_cycle.salary_scale_id.id),
+                ('offer_type', '=', vals.get('offer_type', False) or approval_cycle.offer_type)
+            ], limit=1)
+            if is_exist:
+                raise ValidationError(
+                    _('There is exist approval cycle (%s) with the same filtration criteria.') % is_exist.name)
+        return super(HRSetupApprovalCycle, self).write(vals)
 
 
 class HRSetupApprovalCycleUsers(models.Model):
