8b15d5562fea338aa9dec4d9e14389bce139a6d4
esraa-elmasry <esraa-elmasry@andalusiagroup.net>
2020-09-02 13:03:59 +0200

    wip

--- recruitment_ads/controllers/main.py
+++ recruitment_ads/controllers/main.py
@@ -4,12 +4,11 @@
 # import json
 from odoo import http, tools, _
 from odoo.http import request, Controller
-# import erppeek
+
 class ApproveCycleController(Controller):
 
     @http.route(['/approval/cycle/approved'],csrf=False, type='http', methods=['GET'], auth="public", website=True)
     def approval_cycle_approve(self, **kwargs):
-        # client = erppeek.Client('http://10.24.105.44:8069', 'recruitment_aly', 'admin', 'admin')
         values = list(kwargs.keys())
         data = request.params.copy()
         approval_cycle = request.env['hr.approval.cycle'].sudo().search([('offer_id','=',int(values[0]))],order='create_date desc',limit=1)
@@ -24,26 +23,25 @@ class ApproveCycleController(Controller):
             number_users = len(approval_cycle.users_list_ids)
             for user in approval_cycle.users_list_ids :
                 count += 1
-                # if user.state == 'no_action':
-                #     user.sudo().write({'state' : 'approved'})
-                #     if count < number_users:
-                #         template = request.env.ref('recruitment_ads.approval_cycle_mail_template', False)
-                #         if template:
-                #             template= request.env['mail.template'].sudo().browse(template.id)
-                #             template.email_to = approval_cycle.users_list_ids[count].approval_user_id.email
-                #             template.send_mail(approval_cycle.id)
-                #             approval_cycle.users_list_ids[count ].sudo().write({'sent' : True})
-                #             break;
-                #     else:
-                #         approval_cycle.state = 'approved'
+                if user.state == 'no_action':
+                    user.sudo().write({'state' : 'approved'})
+                    if count < number_users:
+                        template = request.env.ref('recruitment_ads.approval_cycle_mail_template', False)
+                        if template:
+                            template= request.env['mail.template'].sudo().browse(template.id)
+                            template.email_to = approval_cycle.users_list_ids[count].approval_user_id.email
+                            template.send_mail(approval_cycle.id)
+                            approval_cycle.users_list_ids[count ].sudo().write({'sent' : True})
+                            break;
+                    else:
+                        approval_cycle.state = 'approved'
 
-        response = request.render('auth_signup.signup', data)
+        response = request.render('recruitment_ads.hr_approval_cycle_response', data)
         response.headers['X-Frame-Options'] = 'DENY'
         return response
 
     @http.route(['/approval/cycle/reject'], csrf=False, type='http', methods=['GET'], auth="public", website=True)
     def approval_cycle_rejected(self, **kwargs):
-        # client = erppeek.Client('http://10.24.105.44:8069', 'recruitment_aly', 'admin', 'admin')
         values = list(kwargs.keys())
         data = request.params.copy()
         approval_cycle = request.env['hr.approval.cycle'].sudo().search([('offer_id','=',int(values[0]))],order='create_date desc',limit=1)
@@ -61,6 +59,6 @@ class ApproveCycleController(Controller):
                     approval_cycle.state = 'rejected'
                     break;
 
-        response = request.render('auth_signup.signup', data)
+        response = request.render('recruitment_ads.hr_approval_cycle_response', data)
         response.headers['X-Frame-Options'] = 'DENY'
         return response
