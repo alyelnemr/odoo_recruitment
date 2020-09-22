564da4c38af286025c0c66fe9de0c37b0a0c3c58
Aly.Abdelhamid <Aly.Abdelhamid@andalusiagroup.net>
2020-09-22 18:09:11 +0200

    [done] #360010# CR: HR Policy for CEO Approval

--- recruitment_ads/models/policy.py
+++ recruitment_ads/models/policy.py
@@ -2,6 +2,13 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
+class HRPolicyCEOApproval(models.Model):
+    _name = 'hr.policy.ceo.approval'
+
+    approval_group = fields.Many2one('hr.master.approval.group', string='Approval Group', ondelete='cascade')
+    hr_policy = fields.Many2one('hr.policy', string='HR Policy')
+
+
 class HRPolicyOfferAndHire(models.Model):
     _name = 'hr.policy.offer.and.hire.level'
 
@@ -34,29 +41,13 @@ class HRPolicy(models.Model):
     day = fields.Integer(string="Days")
     month = fields.Integer(string="Months")
     year = fields.Integer(string="Years")
+    ceo_approval_amount = fields.Integer(string="Max Offer Amount", default=15000)
+    ceo_approval_group = fields.One2many('hr.policy.ceo.approval', 'hr_policy', string='Approval Group')
     hr_policy_type = fields.Selection([('application_period', 'Application Period'),
-                                       ('offer_and_hire', 'Offer and Hire')], string='Policy Type',
+                                       ('offer_and_hire', 'Offer and Hire'),
+                                       ('ceo_approval_amount', 'CEO Approval Amount')], string='Policy Type',
                                       default='application_period', required=True)
     offer_and_hire_level = fields.One2many('hr.policy.offer.and.hire.level', 'hr_policy', string='Levels')
-
-    # @api.onchange('hr_policy_type')
-    # def onchange_hr_policy_type(self):
-    #     self.name = 'Offer and Hire' if self.hr_policy_type == 'offer_and_hire' else 'Application Period'
-    #     self.offer_and_hire_level.unlink()
-    #     if self.hr_policy_type == 'offer_and_hire':
-    #         values = []
-    #         job_levels = self.env['job.level'].search([])
-    #         for level in job_levels:
-    #             values.append((0, 0, {
-    #                 'level': level.id,
-    #                 'offer': 0,
-    #                 'hire': 0,
-    #                 'total': 0,
-    #                 'hr_policy': self._origin.id
-    #             }))
-    #         self.update({
-    #             'offer_and_hire_level': values,
-    #         })
 
     @api.multi
     def unlink(self):
