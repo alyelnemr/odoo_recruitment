a2a2e8a064bed6288835dfd5f54583251e42fae1
marina.mikhael <Marina.Mikhael@Andalusiagroup.net>
2020-09-24 16:00:47 +0200

    [ADD] Item 359504:View Recruitment Tickets Tab add date

--- recruitment_ads/models/recruitment_tickets.py
+++ recruitment_ads/models/recruitment_tickets.py
@@ -7,9 +7,11 @@ class RecruitmentTickets(models.Model):
     _name = 'recruitment.tickets'
     _inherit = ['mail.thread']
     _description = 'Recruitment Tickets'
+    _order = 'business_unit_id,create_date'
+
     applicant_name = fields.Char(string='Applicant Name', required=True)
     applicant_code = fields.Char(string='Applicant Code', required=True)
-    business_unit_id = fields.Many2one('business.unit', string='Business Unit', required=True)
+    business_unit_id = fields.Many2one('business.unit', string='Business Unit)', required=True)
     job_id = fields.Many2many('hr.job', string='Job Position', required=True)
     department_id = fields.Many2many('hr.department', string='Department', required=True)
     section_id = fields.Many2many('hr.department', string='Section', required=True)
