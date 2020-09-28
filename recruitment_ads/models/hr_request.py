935c16a31e3902616359d931244b51725898ff65
esraa-elmasry <esraa-elmasry@andalusiagroup.net>
2020-09-28 10:09:00 +0200

    change hr request  into list view and  create user account WIP

--- recruitment_ads/models/hr_request.py
+++ recruitment_ads/models/hr_request.py
@@ -16,3 +16,41 @@ class HrRequest(models.Model):
     hr_responsible = fields.Many2one('res.partner', string='Hr Responsible', readonly=True)
     hiring_status = fields.Char()
     hiring_date = fields.Date()
+
+    @api.multi
+    def create_user_account(self):
+        self.ensure_one()
+
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = \
+                ir_model_data.get_object_reference('recruitment_ads',
+                                                   'create_user_account_mail_template')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = \
+                ir_model_data.get_object_reference('recruitment_ads',
+                                                   'view_create_user_account_mail_compose_message_wizard_from')[1]
+        except ValueError:
+            compose_form_id = False
+
+        ctx = {
+            'default_model':'hr.request',
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'time_format': '%I:%M %p',
+            'force_email': True,
+        }
+
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'create.user.account.mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
